a479e5fac4095828de65ea7146586ef937815c31
yassineboujrada <yassine.boujrada@gmail.com>
2023-07-10 15:38:31 +0100

    here all the work for colorize predict edges of the model

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import '../../assets/css/home.css';
 import '../../assets/css/ObjViewer.css';
-import { useNavigate } from "react-router-dom";
+import ObjViewer from './ObjViewer.jsx';
 import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { homeRoute } from "../../utils/ApiRoutes";
+import "react-toastify/dist/ReactToastify.css"
 
 function Home() {
   const toastOptions = {
@@ -14,50 +13,62 @@ function Home() {
     pauseOnHover: true,
     draggable: true,
     theme: "dark",
-  };
-
-  // to set the file to be uploaded
+};
   const [file, setFile] = useState(null);
-  const navigate = useNavigate();
+  const [objUrl, setObjUrl] = useState(null);
+  const [showObjViewer, setShowObjViewer] = useState(false);
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
 
-
-  // send file to backend for prediction and navigate to next page (/home/fileUploaded)
   const handleSubmit = async (event) => {
     event.preventDefault();
     var formData = new FormData();
     formData.append('objFile', file);
+    console.log(file);
     if(file.name.split('.').pop() !== 'obj') {
       toast.error('Please upload a .obj file', toastOptions);
       return;
     }
+    const reader = new FileReader();
+
+    reader.onload = function(event) {
+      const url = URL.createObjectURL(new Blob([event.target.result], { type: 'text/plain' }));
+      setObjUrl(url);
+      setShowObjViewer(true);
+      console.log("here");
+      
+    };
+
+    reader.readAsText(file);
     try {
-      var response = await axios.post(homeRoute, formData, {
+      var response = await axios.post('http://localhost:5000/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
       });
-      toast.success("hi", toastOptions);
-      navigate("/home/fileUploaded", {state:{ file,segmentation:response.data }});
+      console.log(response.data); // Do something with the response data
+      toast.success(response.data.message, toastOptions);
     } catch (error) {
       console.log(error); // Handle any errors
     }
+    // Do something with the formData, like send it to a server
   };
 
   return (
     <>
         <main>
-            <div ><br />
-            <h1 style={{"marginLeft":"60px"}}>Home :</h1><br /><br /><br />
-            <div className='description-holder'>
+            <h1>Home :</h1><br /><br />
+            <div className="aboutus-holder" id="aboutus-holder"><br />
+                <div id="objViewerDiv"></div>
                 <div className='form-holder'>
                     <div className='title-form'> 
                         <h2>Choose a file to upload</h2>
                     </div><br />
+
                     <form onSubmit={handleSubmit}>
-                        <div className="form-group">
+                        <div className="form-group" id>
+                            {showObjViewer && <ObjViewer url={objUrl}/>}
                             <label className="form-label"><h3>Choose a file:</h3></label>
                             <input type="file" className="form-control-file" accept=".obj" onChange={handleFileChange} />
                         </div>
@@ -65,7 +76,6 @@ function Home() {
                             <button type="submit" className="btn btn-primary">Submit</button>
                         </div>
                     </form>
-                </div>
                 </div>
             </div>
         </main>
