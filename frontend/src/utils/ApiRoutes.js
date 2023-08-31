f8376ef40f27b35b066410f49b5c9ed4a1d039aa
boujrada yassine <yassine.boujrada@gmail.com>
2023-08-31 01:45:47 +0100

    dataset for train model

--- frontend/src/utils/ApiRoutes.js
+++
@@ -1,6 +0,0 @@
-export const host = "https://f19f-35-247-111-183.ngrok-free.app";
-export const loginRoute = `${host}/api/auth/login`;
-export const registerRoute = `${host}/api/auth/register`;
-export const homeRoute = `${host}/api/auth/upload`;
-export const loadRoute = `${host}/api/auth/models/`;
-export const settingRoute = `${host}/api/auth/setting`;
